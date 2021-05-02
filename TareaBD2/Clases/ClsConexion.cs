38403a94338070ba4af6d9ee89aba1887ef11e6a
ANNER RONALDO ESCOBAR CRUZ <aescobarc13@miumg.edu.gt>
2021-05-01 18:19:00 -0600

    Agregar archivos de proyecto.

---
+++ TareaBD2/Clases/ClsConexion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace TareaBD2.Clases
+{
+    class ClsConexion
+    {
+        public SqlConnection conexion;
+        private String _conexion { get; }
+
+        public  ClsConexion()
+        {
+
+            _conexion = "Data Source=LT-RULDIN\\SQLEXPRESS2014;Initial Catalog=programacion1;Integrated Security=True";
+
+        }
+
+        /// <summary>
+        /// Cierra la conexion.
+        /// </summary>
+        public void cerrarConexionBD()
+        {
+            conexion.Close();
+        }
+
+
+
+        /// <summary>
+        /// abre la conexion
+        /// </summary>
+        public void abrirConexion()
+        {
+            conexion = new SqlConnection(_conexion);
+            conexion.Open();
+        }
+
+    }    
+}
---
+++ TareaBD2/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace TareaBD2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(51, 40);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(165, 23);
+            this.button.TabIndex = 0;
+            this.button.Text = "Cargar Archivo a MSQL";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 450);
+            this.Controls.Add(this.button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+    }
+}
+
