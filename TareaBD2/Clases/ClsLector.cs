bb7cd6f626ba282fa5e43e9f9f262484ce7cdf3a
ANNER RONALDO ESCOBAR CRUZ <aescobarc13@miumg.edu.gt>
2021-05-01 18:23:25 -0600

    Prueba 2

--- TareaBD2/Clases/ClsLector.cs
+++ TareaBD2/Clases/ClsLector.cs
@@ -10,29 +10,29 @@ namespace TareaBD2.Clases
     class ClsLector
     {
 
-        //public void Lector()
-        //{
-        //    using (SqlConnection conn = new SqlConnection(@"PAT DE LA CONEXION"))
-        //    {
+        public void Lector()
+        {
+            using (SqlConnection conn = new SqlConnection(@"PAT DE LA CONEXION"))
+            {
 
-        //        conn.Open();
-        //        using (StreamReader Reader = new StreamReader(@"C: \Users\anner\Desktop\Universidad Mariano Galvez\Tercer Semestre\Programacion 1\datosTexto.csv"))
-        //        {
-        //            while (!Reader.EndOfStream)
-        //            {
-        //                var line = Reader.ReadLine();
-        //                var values = line.Split(';');
-        //                var sql = "INSERT INTO PENDIENTE VALUES (" + values[0] + ", " + values[1] + ", " + values[2] + ", " + values[3] + ", " + values[4] + " )";
+                conn.Open();
+                using (StreamReader Reader = new StreamReader(@"C: \Users\anner\Desktop\Universidad Mariano Galvez\Tercer Semestre\Programacion 1\datosTexto.csv"))
+                {
+                    while (!Reader.EndOfStream)
+                    {
+                        var line = Reader.ReadLine();
+                        var values = line.Split(';');
+                        var sql = "INSERT INTO PENDIENTE VALUES (" + values[0] + ", " + values[1] + ", " + values[2] + ", " + values[3] + ", " + values[4] + " )";
 
-        //                var cmd = new SqlCommand();
-        //                cmd.CommandText = sql;
-        //                cmd.CommandType = System.Data.CommandType.Text;
-        //                cmd.Connection = conn;
-        //                cmd.ExecuteNonQuery();
+                        var cmd = new SqlCommand();
+                        cmd.CommandText = sql;
+                        cmd.CommandType = System.Data.CommandType.Text;
+                        cmd.Connection = conn;
+                        cmd.ExecuteNonQuery();
 
-        //            }
-        //        }
-        //        conn.Close();
+                    }
+                }
+                conn.Close();
             }
         }
 
